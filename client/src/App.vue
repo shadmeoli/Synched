f71f5c50f6c10b1d5bed8682a364e436163949c5
shadmeoli <shadcodes@gmail.com>
2023-01-15 18:18:33 +0300

    Client up date

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,92 +1,21 @@
 <template>
-  <v-app dark>
-    <!-- adding the nav bar for all screes -->
-    <v-app-bar
-        color="pink"
-        elevation=0
-      >
-        <v-app-bar-title>SYNCHED</v-app-bar-title>
-
-        <template v-slot:append>
-
-          <v-btn icon elevation=5 @click.stop="drawer = !drawer">
-            <v-avatar
-              color="white"
-              size="large"
-            >
-            <v-icon>mdi-account</v-icon>
-          </v-avatar>
-          </v-btn>
-        </template>
-
-      </v-app-bar>
-      <v-navigation-drawer
-        v-model="drawer"
-        class="gray"
-        theme="light"
-        temporary
-      >
-            <v-btn variant="plain" height=45 class="mb-2" block elevation=0>
-              saved playlist
-            </v-btn>
-            <v-btn variant="plain" height=45 class="mb-2" block elevation=0>
-              Create playlist
-            </v-btn>
-            <v-btn variant="plain" height=45 class="mb-2" block elevation=0>
-              About us
-            </v-btn>
-            <v-btn variant="plain" height=45 class="mb-2" block elevation=0>
-              Profile
-            </v-btn>
-          
-
-        <template v-slot:append>
-          <!-- action -->
-          <div class="pa-2">
-            <v-btn height=45 class="mb-2" block color="pink" elevation-0>
-              Signup
-            </v-btn>
-            <v-btn height=45 variant="outlined" block color="pink" elevation-0>
-              Logout
-            </v-btn>
-
-            <!-- url redirect buttons -->
-            <v-btn-group dense>
-              <v-btn>
-                <v-icon>mdi-github</v-icon>
-              </v-btn>
-
-              <v-btn>
-                <v-icon>mdi-twitter</v-icon>
-              </v-btn>
-
-              <v-btn>
-                <v-icon>mdi-instagram</v-icon>
-              </v-btn>
-
-              <v-btn>
-                <v-icon>mdi-facebook</v-icon>
-              </v-btn>
-            </v-btn-group>
-          </div>
-        </template>
-        
-      </v-navigation-drawer>
-
+  <v-app>
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
   name: 'App',
 
-  data: () => ({
-    //
-    drawer: null,
-  }),
-}
+  data () {
+    return {
+      //
+    }
+  },
+})
 </script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <search />
+  <hello-world />
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
-import search from '../components/search.vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    search,
+    HelloWorld,
   },
 });
 </script>
