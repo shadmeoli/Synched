d702c5ba13e4ad2fb1fabc3df47f0c0b71c8d216
shadmeoli <shadcodes@gmail.com>
2023-01-20 16:59:41 +0300

    App full update

---
+++ client/src/views/MyPlaylist.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is the my playlist page</h1>
+  </div>
+</template>
+
+<script>
+  
+</script>
---
+++ client/src/views/SavedPlaylist.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is the saved playlist page</h1>
+  </div>
+</template>
+
+<script>
+  
+</script>
